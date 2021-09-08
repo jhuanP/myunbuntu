6ad0b695df3c469affa5e16f813ba46f83d36045
jhuanP <j.pressley87@hotmail.com>
2021-09-08 19:48:18 -0400

    code changes

--- react-things/github-finder/src/App.js
+++ react-things/github-finder/src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
   
   //search github users from search
    searchUsers = async (text) => {
+    this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data.items, loading: false});
