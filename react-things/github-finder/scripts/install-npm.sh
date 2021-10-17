44a14db70b85d6721085c04fb5b6968710f9a201
jhuanP <j.pressley87@hotmail.com>
2021-10-17 11:37:21 -0400

    adding nvm commands to install script

--- react-things/github-finder/scripts/install-npm.sh
+++ react-things/github-finder/scripts/install-npm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo yum update && \
+sudo yum update -y && \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && \
 . ~/.nvm/nvm.sh && \
 nvm install  && \
